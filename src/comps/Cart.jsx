85bc23a91da19790abc39bec253b6532b91ba212
Elad Abutbul <eladabutbul61@gmail.com>
2023-04-20 17:36:18 +0300

    frontend and backend basic

---
+++ src/comps/Cart.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Cart() {
+  return <div>
+    <h1>my cart:</h1>
+    <button>pay all</button>
+  </div>;
+}
---
+++ src/comps/Search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div>
+      <h1>search</h1>
+      <input type="text" placeholder="search.." />
+    </div>
+  );
+}
